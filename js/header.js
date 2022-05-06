1e645886fb4f38c654112fe119fec6881f0ee92a
ElizabethPeredo <61327189+ElizabethPeredo@users.noreply.github.com>
2022-05-06 01:53:27 -0500

    Inversión1

--- js/header.js
+++ js/header.js
@@ -1,14 +1,11 @@
-document.getElementById("header").innerHTML =
-`
+document.getElementById("header").innerHTML = `
+
 <div class="navbar__container">
 
 <div class="navbar__logo">
     <a href="https://elizabethperedo.github.io/SERFOR/">
-
         <img src="https://www.serfor.gob.pe/portal/wp-content/uploads/2021/01/midagri-serfor.png" alt=""
-            class="navbar__logo-img" title="SERFOR">
-            
-    </a>
+            class="navbar__logo-img" title="SERFOR"></a>
 </div>
 
 <div class="navbar__items">
@@ -16,13 +13,11 @@ document.getElementById("header").innerHTML =
         <a href="https://elizabethperedo.github.io/SERFOR/">Inicio</a>
 
         <!--AQUI AGREGAR LOS LINK-->
-        <a href="../anexo1.html">Anexo 1</a>
-
+        
     </ul>
 </div>
 <div class="navbar__icon">
     <i class="fas fa-bars" id="menu-bars"></i>
 </div>
 </div>
-`
-;
+`;
